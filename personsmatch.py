0024fa4823908cf6d51ce916be0746fe99b48ac2
Khwilo Kabaka <kwhyzzy@gmail.com>
2017-10-18 13:38:34 +0300

    Create a method for retrieving the Twitter client credentials.

--- personsmatch.py
+++ personsmatch.py
@@ -6,19 +6,25 @@ import json
 import twitter
 from watson_developer_cloud import PersonalityInsightsV2 as PersonalityInsights
 
-# Set the environment variables.
-twitter_consumer_key = os.environ['PERSONALITY_MATCH_CONSUMER_KEY']
-twitter_consumer_secret = os.environ['PERSONALITY_MATCH_CONSUMER_SECRET']
-twitter_access_token = os.environ['PERSONALITY_MATCH_ACCESS_TOKEN']
-twitter_access_secret = os.environ['PERSONALITY_MATCH_ACCESS_SECRET']
+def get_twitter_client():
+  try:
+    # Set the environment variables.
+    twitter_consumer_key = os.environ['PERSONALITY_MATCH_CONSUMER_KEY']
+    twitter_consumer_secret = os.environ['PERSONALITY_MATCH_CONSUMER_SECRET']
+    twitter_access_token = os.environ['PERSONALITY_MATCH_ACCESS_TOKEN']
+    twitter_access_secret = os.environ['PERSONALITY_MATCH_ACCESS_SECRET']
+  except KeyError:
+    sys.stderr.write("TWITTER_* environment variables not set\n")
+    sys.exit(1)
 
-# Create an instance of the Twitter API
-twitter_api = twitter.Api(consumer_key=twitter_consumer_key, consumer_secret=twitter_consumer_secret, access_token_key=twitter_access_token, access_token_secret=twitter_access_secret)
+  # Create an instance of the Twitter API
+  twitter_api = twitter.Api(consumer_key=twitter_consumer_key, consumer_secret=twitter_consumer_secret, access_token_key=twitter_access_token, access_token_secret=twitter_access_secret)
 
+  return twitter_api
 
 # Retrieve data from Twitter
 handle = "@khwilo"
-statuses = twitter_api.GetUserTimeline(screen_name=handle, count=200, include_rts=False)
+statuses = get_twitter_client().GetUserTimeline(screen_name=handle, count=200, include_rts=False)
 
 # View the results
 for status in statuses:
