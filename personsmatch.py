06163e85c8643ff77ff84decb3b3144d80b7e457
Khwilo Kabaka <kwhyzzy@gmail.com>
2017-10-18 12:50:27 +0300

    Create an instance of the Twitter API.

--- personsmatch.py
+++ personsmatch.py
@@ -6,7 +6,11 @@ import json
 import twitter
 from watson_developer_cloud import PersonalityInsightsV2 as PersonalityInsights
 
+# Set the environment variables.
 twitter_consumer_key = os.environ['PERSONALITY_MATCH_CONSUMER_KEY']
 twitter_consumer_secret = os.environ['PERSONALITY_MATCH_CONSUMER_SECRET']
 twitter_access_token = os.environ['PERSONALITY_MATCH_ACCESS_TOKEN']
 twitter_access_secret = os.environ['PERSONALITY_MATCH_ACCESS_SECRET']
+
+# Create an instance of the Twitter API
+twitter_api = twitter.Api(consumer_key=twitter_consumer_key, consumer_secret=twitter_consumer_secret, access_token_key=twitter_access_token, access_token_secret=twitter_access_secret)
