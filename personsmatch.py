325a79ca43b4d4e4aa670b7b1d15a295f0d2eb9d
Khwilo Kabaka <kwhyzzy@gmail.com>
2017-10-18 12:39:25 +0300

    Set the Twitter environment variables.

--- personsmatch.py
+++ personsmatch.py
@@ -1,6 +1,12 @@
+import os
 import sys
 import operator
 import requests
 import json
 import twitter
 from watson_developer_cloud import PersonalityInsightsV2 as PersonalityInsights
+
+twitter_consumer_key = os.environ['PERSONALITY_MATCH_CONSUMER_KEY']
+twitter_consumer_secret = os.environ['PERSONALITY_MATCH_CONSUMER_SECRET']
+twitter_access_token = os.environ['PERSONALITY_MATCH_ACCESS_TOKEN']
+twitter_access_secret = os.environ['PERSONALITY_MATCH_ACCESS_SECRET']
